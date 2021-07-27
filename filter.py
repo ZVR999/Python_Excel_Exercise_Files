bb802e4e3a4a917ecf701dd52f8a7f7602f7d153
VR <zachkery999@gmail.com>
2021-07-27 15:00:25 -0500

    Practice
    Tried out look ups on columns and within columns. Practiced creating excel files off csv file

--- filter.py
+++ filter.py
@@ -4,5 +4,16 @@ df = pd.read_csv('Names.csv', header=None)
 
 df.columns = ['First','Last','Address','City','State','Area Code','Income']
 
+# print(df)
 
-print(df.loc[(df['First'] == 'Stephen') & (df['City'] == 'SomeTown')])
+# print(df.loc[df['First'] == 'Jack'])
+
+# print(df.loc[df['Income'] > 20000])
+
+# print(df['State'][1:3])
+
+print(df.iloc[1])
+
+values_to_save = df.iloc[1]
+
+saved_Values = values_to_save.to_excel('Values_to_save_Test1.xlsx', index=None)
