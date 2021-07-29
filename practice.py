0139e773d9907fc4b066518057d002f987b91af6
VR <zachkery999@gmail.com>
2021-07-29 09:44:11 -0500

    Headers and column
    Created headers for a csv file that did not have any and drop a column

--- practice.py
+++ practice.py
@@ -6,21 +6,28 @@ Created on Tue Jul 27 15:03:53 2021
 """
 
 import pandas as pd
+import numpy as np
+from openpyxl.workbook import Workbook
 
-df = pd.read_excel('Modified.xlsx')
+# df = pd.read_excel('Modified.xlsx')
 
-print(df)
+# print(df)
 
-# print(df.iloc[1])
+# # print(df.iloc[1])
 
-df['Test Col'] = False
-
-print(df)
-
-
-
+# df['Test Col'] = False
 
 # print(df)
 
 # print(df.loc[df[1] == 'Jack'])
 
+df = pd.read_csv('Names.csv', header=None)
+
+# Creat column headers since the csv file doesn't have any
+df.columns = ['First','Last','Address','City','State','Area Code', 'Income']
+
+# Drop the address column
+df.drop(columns='Address', inplace=True)
+
+# Check to make sure there are no issues
+# print(df)
