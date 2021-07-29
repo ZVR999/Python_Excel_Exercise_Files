e7f91d749342446392fcce2b926f915192923747
VR <zachkery999@gmail.com>
2021-07-29 10:01:32 -0500

    Set index and search within index
    Used .loc[] to search within the data frame with the 'Area Code' column being used as the index.

--- practice.py
+++ practice.py
@@ -31,3 +31,15 @@ df.drop(columns='Address', inplace=True)
 
 # Check to make sure there are no issues
 # print(df)
+
+df = df.set_index('Area Code')
+
+#Used the Area Code column as an index and displayed the information of that area code
+# print(df.loc[8074])
+
+# Used an ":" to start at 8074 and continue through out that column until the end while 
+# displaying the "First" column as well
+# print(df.loc[8074:, 'First'])
+
+# Repeated the above but with different inputs to get a feel for locating data with pandas
+print(df.loc[9119:298, 'Last'])
